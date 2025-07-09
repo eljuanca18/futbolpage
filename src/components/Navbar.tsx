ef0e4ed7488303084d23061ab6db39ed26652dca
Juan Goez <camilojuangb18@gmail.com>
2025-07-09 11:26:16 -0500

    lpn

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+import { Navbar as BsNavbar, Nav, Container } from "react-bootstrap";
+
+export default function Navbar() {
+  return (
+    <BsNavbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Link href="/" className="navbar-brand">FutbolStats</Link>
+        <Nav className="me-auto">
+          <Link href="/equipos" className="nav-link">Equipos</Link>
+          <Link href="/jugadores" className="nav-link">Jugadores</Link>
+        </Nav>
+      </Container>
+    </BsNavbar>
+  );
+}
